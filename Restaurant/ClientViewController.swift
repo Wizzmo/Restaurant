f87a5aaac7988957eac7de50ce3792d7f94cad3d
Maxim Makarov <makarovmaxqa@gmail.com>
2024-01-09 21:09:32 +0500

    Prepare project

--- Restaurant/ClientViewController.swift
+++ Restaurant/ClientViewController.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ClientViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
